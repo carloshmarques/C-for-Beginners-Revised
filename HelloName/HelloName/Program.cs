71378c6a37a9505413c3fa1b4a2e9d27099d2869
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-03-07 12:27:55 +0000

    added first project

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}; ");
+            Console.ReadKey();
+
+        }
+    }
+}
