cea4985710fad66dbe4269a15340900fa451d63a
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-03-09 11:20:25 +0000

    Finished readme.md for now

--- HelloName/HelloName/Program.cs
+++ HelloName/HelloName/Program.cs
@@ -14,16 +14,16 @@ namespace HelloName
             // https://youtu.be/2pquQMSYk6c?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di
             // Set the console window title.//
             Console.Title = "Hello Name";
-            //Console.ReadKey();
+            string name = "";
             // Display a greeting to the user.//
             // Ask the user for their name and then greet them by name.//
             Console.WriteLine("Hello, what is your name?");
-            var name = Console.ReadLine();
+            name = Console.ReadLine();
             // Greet the user by name.//
-            Console.WriteLine($"Hello, {name}");
+            Console.WriteLine($"Hello, {name}!");
             // Wait for the user to acknowledge the results.//
             Console.ReadKey();
-            // Contnue here: https://youtu.be/2pquQMSYk6c?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=768
+            // Contnue here: https://youtu.be/2pquQMSYk6c?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=818
         }
     }
 }
