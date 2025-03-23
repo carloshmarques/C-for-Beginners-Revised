9dc5db2132a8bdfad786efa9d89aef2551016c7f
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-03-23 13:42:14 +0000

    C# Variables Tutorial
    started part 3 of the tutorial

---
+++ C# Variables Tutorial/StudentApplication/StudentApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // link: https://youtu.be/rM9HostBLJ4?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di
+            /*
+             * 
+             */
+        }
+    }
+}
---
+++ C# Variables Tutorial/StudentApplication/StudentApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StudentApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b57f9a3-306c-4e28-9839-662e825b0af5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
