6029b67df2fcfb2444b36f69435a033c903f20fb
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-14 14:22:19 +0100

    Finished C# Constants and Readonly Variables Tutorial

--- MarsInfoAplication/MarsInfoAplication/Program.cs
+++ MarsInfoAplication/MarsInfoAplication/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
 
 namespace MarsInfoAplication
-{
-    internal class Program
+{     
+    public class Constants
     {
-        static void Main(string[] args)
+        public const int SpeedOfLight = 300000;
+    }
+    public class Mars
+    {
+      
+        public readonly int DistanceToMars = 0;
+
+        public Mars()
         {
+            DistanceToMars = Convert.ToInt32(ConfigurationManager.AppSettings["DistanceToMars"]);
+            // Continue here:https://youtu.be/yvOdN5PBY2g?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=610
+
         }
+        public int GetInfoTravelTime()
+        {
+             return DistanceToMars/Constants.SpeedOfLight;
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Mars mars = new Mars();
+
+                Console.WriteLine($"Information: Travel time of light, is aproximatilly {mars.GetInfoTravelTime()} seconds from mars to earth!");
+                Console.ReadKey();
+        }
+
+        }
+       
     }
 }
