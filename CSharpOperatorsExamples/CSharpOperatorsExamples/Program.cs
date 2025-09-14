4c96c7d4de21ce2af57220cf75599f00a6a50b7f
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-14 20:34:35 +0100

    Started C Sharp Operators Tutorial

---
+++ CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOperatorsExamples
+{
+    internal class Program
+    {
+        // link: https://youtu.be/qGgwm95FK5M?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=25
+        static void Main(string[] args)
+        {
+        }
+    }
+}
