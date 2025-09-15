d518ede495f4f589b2645419b823e3163680f8b2
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-15 17:31:35 +0100

    Cloned Csharp Operators Documentation repo into relevant docs

--- CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
+++ CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
@@ -11,6 +11,39 @@ namespace CSharpOperatorsExamples
         // link: https://youtu.be/qGgwm95FK5M?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=25
         static void Main(string[] args)
         {
+            // Unary  Operators
+            //++
+            string line = string.Empty;
+            string exampleLabel = string.Empty;
+
+            int a = 1;
+            a++;
+            //a equals 2
+            exampleLabel = "$1: ";
+            line =$"{exampleLabel} 'a' equals {a}"; 
+            LogToScreen(line);
+
+            bool b = true;
+            bool c = !b;
+            // c equals false
+            exampleLabel = "$2: ";
+            line = $"{exampleLabel} 'c' equals {c}";
+            LogToScreen(line);
+
+            // Multiplicative
+            //*
+            int d = 5;
+            int e = d * 2;
+            exampleLabel = "$3: ";
+            line = $"{exampleLabel} 'e' equals {e}";
+            LogToScreen(line);
+
+            // continue here:https://youtu.be/qGgwm95FK5M?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=244
         }
+        public static void LogToScreen(string line)
+        {
+            Console.WriteLine(line);
+        }
+
     }
 }
