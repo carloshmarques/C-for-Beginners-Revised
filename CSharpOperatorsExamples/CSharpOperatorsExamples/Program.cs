cc8cc753d8f7f9f6cefa207cc29680d5c0b7e9cf
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-15 17:42:20 +0100

    All for now

--- CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
+++ CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
@@ -11,33 +11,71 @@ namespace CSharpOperatorsExamples
         // link: https://youtu.be/qGgwm95FK5M?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=25
         static void Main(string[] args)
         {
-            // Unary  Operators
+            //Unary Operators
+
             //++
+
+            //E1
             string line = string.Empty;
             string exampleLabel = string.Empty;
 
             int a = 1;
             a++;
             //a equals 2
-            exampleLabel = "$1: ";
-            line =$"{exampleLabel} 'a' equals {a}"; 
+            exampleLabel = "E1: ";
+            line = $"{exampleLabel}'a' equals {a}";
+
             LogToScreen(line);
 
+            //!
+
+            //E2
             bool b = true;
             bool c = !b;
-            // c equals false
-            exampleLabel = "$2: ";
-            line = $"{exampleLabel} 'c' equals {c}";
+            //c equals false
+            exampleLabel = "E2: ";
+            line = $"{exampleLabel}'c' equals {c}";
+
             LogToScreen(line);
 
-            // Multiplicative
+            //Multiplicative
             //*
+
+
+            //E3
             int d = 5;
             int e = d * 2;
-            exampleLabel = "$3: ";
-            line = $"{exampleLabel} 'e' equals {e}";
+            //e equals 10
+            exampleLabel = "E3: ";
+            line = $"{exampleLabel}'e' equals {e}";
+
             LogToScreen(line);
 
+            // /
+
+            //E4
+            int f = 10;
+            int g = f / 2;
+            //g equals 5
+            exampleLabel = "E4: ";
+            line = $"{exampleLabel}'g' equals {g}";
+
+            LogToScreen(line);
+
+            // %
+
+            //E5
+            int h = 10;
+            int i = h % 3;
+            //i equals 1
+            exampleLabel = "E5: ";
+            line = $"{exampleLabel}'i' equals {i}";
+
+            LogToScreen(line);
+
+            //Additive Operators
+
+            // docs: https://github.com/GavinLonDigital/CSharpOperatorExamples/blob/master/CSharpOperatorExamples/Program.cs
             // continue here:https://youtu.be/qGgwm95FK5M?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=244
         }
         public static void LogToScreen(string line)
