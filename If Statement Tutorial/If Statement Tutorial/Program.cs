16fb6b3a510170916d9d340a11003671c312dc01
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-17 20:53:19 +0100

    C# If Statement Tutorial

---
+++ If Statement Tutorial/If Statement Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement_Tutorial
+{      // Link https://youtu.be/2mChNV9GmpM?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&t=93
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
