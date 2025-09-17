4c66a8010ca057e0bdafc372f5a773dff2882e8e
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-17 20:50:51 +0100

    Started finish Operator Overload Example

--- CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
+++ CSharpOperatorsExamples/CSharpOperatorsExamples/Program.cs
@@ -356,7 +356,7 @@ namespace CSharpOperatorsExamples
         public static void LogToScreen(string line)
         {
             Console.WriteLine(line);
-            Console.ReadKey();
+           
         }
 
     }
---
+++ OperatorOverloadingExample/OperatorOverloadingExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingExample
+{
+    internal class Program
+    {
+        public class Rectangle
+        {
+            public int Width = 0;
+            public int Height = 0;
+
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+
+            }
+            public static Rectangle operator +(Rectangle rect1, Rectangle rect2)
+            {
+                Rectangle rectResult = new Rectangle(rect1.Width + rect2.Width, rect1.Height + rect2.Height);
+
+                return rectResult;
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle(12, 10);
+            Rectangle rect2 = new Rectangle(5, 2);
+
+            Rectangle rectResult = rect1 + rect2;
+
+            Console.WriteLine($"'rect1(12,10)' + 'rect2(5,2)' = 'rectResult({rectResult.Width},{rectResult.Height})'");
+
+            Console.ReadKey();
+        }
+    }
+}
